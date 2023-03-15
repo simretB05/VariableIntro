caca549ef3abb56d774dd28754605f9d7516abc6
simretB05 <happysimret@gmail.com>
2023-03-14 18:34:15 -0600

    connected app.js file to index.html and added declared 4 variables, 2numbers and 2 strings

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let my_name = "green";
+let mom ="simret"
+let my_lapTop = "hp"
+
+let age = 67;
+let RAM = 8;
+let arrival_date = "2020/01/22";
+let departure_date = "2023/02/22";
+let my_color = "green";
+let my_car = "nissan";
+let age_ram = age+RAM
+console.log( age )
+console.log( my_name )
+console.log( my_lapTop )
+console.log( age )
+console.log( RAM )
+console.log( arrival_date )
+console.log( departure_date )
+console.log( my_car )
+console.log( my_color )
+console.log( mom )
+console.log(age_ram)
